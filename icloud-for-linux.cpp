c0df28c367b38e1e9d9937d67034da7acf1e5542
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2020-08-08 17:32:00 +0100

    Add qmake project

---
+++ icloud-for-linux.cpp
@@ -0,0 +1,16 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/icloud-for-linux.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
---
+++ icloud-for-linux.pro
@@ -0,0 +1,3 @@
+QT += quick
+SOURCES += icloud-for-linux.cpp
+RESOURCES += icloud-for-linux.qrc
