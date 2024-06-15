b2e3dfa27dbaf07903b248aa8fd103223c61acbb
Marcus Tomlinson <themarcustomlinson@gmail.com>
2024-06-15 19:28:38 +0100

    Native WebView implementation (#75)

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <gui/choc_WebView.h>
+#include <gui/choc_DesktopWindow.h>
+#include <gui/choc_MessageLoop.h>
+
+int main(int, char **argv)
+{
+    choc::ui::DesktopWindow appWin(choc::ui::Bounds{0, 0, 0, 0});
+    appWin.centreWithSize(1000, 600);
+    appWin.setWindowTitle( "iCloud " + std::string(argv[2]) );
+    appWin.windowClosed = []()
+    { choc::messageloop::stop(); };
+
+    choc::ui::WebView webView;
+    webView.navigate("https://www.icloud.com/" + std::string(argv[1]));
+
+    appWin.setContent(webView.getViewHandle());
+    appWin.toFront();
+
+    choc::messageloop::run();
+    return 0;
+}
